1aaf483af4ba870aa00bcb547923c70e8d69dd15
ghaar-a <xandbox99@gmail.com>
2026-02-03 15:03:11 -0300

    Bootcamp e Dev

---
+++ src/br/com/dio/challenge/domain/Mentoria.java
@@ -0,0 +1,37 @@
+package br.com.dio.challenge.domain;
+
+import java.time.LocalDate;
+
+//classe filha
+public class Mentoria extends Conteudo {
+
+    private LocalDate data;
+
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO + 20d;
+    }
+
+    public Mentoria() {
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
+
+}
