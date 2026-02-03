808edb167b187808223a0e06b7ebe71e1099196f
ghaar-a <xandbox99@gmail.com>
2026-02-03 12:56:03 -0300

    Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
+import br.com.dio.challenge.domain.Curso;
+import br.com.dio.challenge.domain.Mentoria;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição do curso");
+        curso1.setCargaHoraria(8);
 
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso python");
+        curso2.setDescricao("descrição curso python");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
     }
 }
