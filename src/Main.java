3ce5b68c7b8697a5d10529f327ad012f7d3dc53a
ghaar-a <xandbox99@gmail.com>
2026-02-03 15:01:14 -0300

    Bootcamp e Dev

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.challenge.domain.Curso;
-import br.com.dio.challenge.domain.Mentoria;
+import br.com.dio.challenge.domain.*;
 
 import java.time.LocalDate;
 
@@ -21,5 +20,41 @@ public class Main {
         mentoria.setTitulo("mentoria java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
+
+        Conteudo conteudo = new Curso();  //polimorfismo
+        conteudo.setTitulo("Segurança em Java");
+        conteudo.setDescricao("descrição de segurança em java");
+        Conteudo conteudo1 = new Mentoria();
+        conteudo1.setTitulo("Introdução à ciberseguraç");
+        conteudo1.setDescricao("descrição de introdução à cibersegurança");
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev = new Dev();
+        dev.setNome("John");
+        dev.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + dev.getConteudosInscritos());
+        System.out.println("=");
+        dev.progredir();
+        dev.progredir();
+        System.out.println("Conteúdos inscritos: " + dev.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos: " + dev.getConteudosConcluidos());
+
+
+        System.out.println("===============");
+        Dev dev1 = new Dev();
+        dev1.setNome("Mary");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("=");
+        System.out.println("Conteúdos inscritos: " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        System.out.println("Conteúdos inscritos: " + dev1.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos: " + dev1.getConteudosConcluidos());
     }
 }
